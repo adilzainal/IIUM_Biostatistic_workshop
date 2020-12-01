bd2c8d1da33b3b80769e2b523a5126214cb2dc3d
edreaidid <edreaidid@iium.edu.my>
2020-12-02 00:03:40 +0800

    edited

--- EA2020 analytical.R
+++ EA2020 analytical.R
@@ -10,11 +10,17 @@
 
 #independent sample t test
 t.test(sbp ~ sex, var.equal=TRUE, data=healthstat)
-  
+library(ggpubr)
+p <- ggboxplot(healthstat, x = "sex", y = "sbp",color = "sex", palette = "jco", add = "jitter")
+p + stat_compare_means(method = "t.test")
+
 #one way ANOVA
 
 one.way =aov(sbp ~ exercise, data = healthstat)
 summary(one.way)
+
+j <- ggboxplot(healthstat, x = "exercise", y = "sbp",color = "exercise", palette = "jco", add = "jitter")
+j + stat_compare_means(method = "anova")
 
 tukey.one.way<-TukeyHSD(one.way) #assuming equal variance
 tukey.one.way
@@ -26,6 +32,7 @@ t.test(healthstat$wt, healthstat$wt2, paired=TRUE)
 #pearson correlation coefficient test
 
 cor.test(healthstat$age,healthstat$sbp, method="pearson")
+ggscatter(healthstat, x = "age", y = "sbp", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Age (years)", ylab = "SBP (mmHg)")
 
 #------------------------------------------
 #comparing numerical values: non-parametric
@@ -58,17 +65,18 @@ chisq.test(healthstat$sex,healthstat$smoking,correct=F)
 
 #fisher's exact test (used when more than 20% celss with expected count less than 5)
 
-chisq.test(healthstat$exercise,healthstat$smoking)$expected
-fisher.test(healthstat$exercise,healthstat$smoking)
+chisq.test(healthstat$bmistat,healthstat$smoking)$expected
+fisher.test(healthstat$bmistat,healthstat$smoking)
 
 ---------------------------------------
 #reporting your findings in table form
 ---------------------------------------
 #package needed
 install.packages("sjPlot")
-install.packages("stargazer")
+install.packages("apaTables")
 library(sjPlot)
-library(stargazer)
+library(apaTables)
 
-# contingency table created in word file in your directory!
+#table created in word file in your directory!
 sjt.xtab(healthstat$sex, healthstat$smoking, file = "sjt_contingency.doc")
+apa.aov.table(one.way, filename="Table_anova.doc", table.number = 2)
