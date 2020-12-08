a55475f587e8d61777415d99f1002091f4bc5c63
adilzainal <70354345+adilzainal@users.noreply.github.com>
2020-12-08 11:36:44 +0800

    edited

--- MAZA_Logreg.R
+++ MAZA_Logreg.R
@@ -1,38 +1,48 @@
-## Simple and multiple logistic regression
+# Required package
 install.packages("aod")
+install.packages("dplyr")
+install.packages("ggplot")
 library(aod)
+library(dplyr)
+library(ggplot)
+library(tidyverse)
+library(broom)
+library(car)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Data preparation
 # Make hypertension or composite variable
-sbpc <- cut(sbp , breaks = c(-Inf,139.9,Inf), labels = c("1","2"))
-sbpc <-as.numeric(sbpc)
-dbpc <- cut(dbp , breaks = c(-Inf,89.9,Inf), labels = c("1","2"))
-dbpc <-as.numeric(dbpc)
-bpc <- dbpc + sbpc
-hpt <- cut(bpc, breaks = c(-Inf,2,Inf), labels = c("Normotensive","Hypertension"))
+hpt<-(sbp>=140|dbp>=90)
 hpt <- as.factor(hpt)
 summary(hpt)
 
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#Simple logistic regression
 # Now logistic regression is the appropriate regression analysis when the dependent is dichotomous or binary 
 # This is very useful because majority of our medical outcome is in binary such as mortality or hypertension
 # It is useful when we want to calculate the probability.
 # In r we will use glm function which is generally used to fit generalized linear models to fit the logistic regression model.
 # We need to make it clear that you want to fit a logistic regression model. We resolve this by setting the family argument to binomial. This way, you tell glm() to put fit a logistic regression model instead of one of the many other models that can be fit to the glm.
 contrasts(hpt)
+contrasts(exercise)
 # So we know the reference category is normotensive, so we want to predict or the odds of having hypertension
 # So now we can run the simple logistic regression
 logmodel <- glm(hpt ~ exercise, data=data, family=binomial)
 summary(logmodel)
 
 # The coefficient in logistic regression is in the log odds of the Hpt for a unit increase in predictor exercise
-# Having moderate exercise compare to low exercise, change the log odds of hypertension by -2.1762
+# Having high exercise compare to low exercise, change the log odds of hypertension by -2.9414
 # We can test the overall effect of exercise using the wald.test in the aod library
 wald.test(b = coef(logmodel), Sigma = vcov(logmodel), Terms = 2:3)
-# The p value for the ovrall variable exercise is significant a p<0.05
+# The p value for the overall variable exercise is significant a p<0.05
 
 # We then need to exponentiating the coefficient from log odds into odds 
 exp(coef(logmodel))
 # We also can get the confidence interval using the confint command
 exp(confint(logmodel))
-# Now we can interpret that high exercise have 0.15 lower odds of having hypertension with the 95% CI (0.07,0.33)
+# Now we can interpret that high exercise have 0.05 lower odds of having hypertension with the 95% CI (0.002,0.277)
 
 # We can now see the model fit, first by the significance of the overall model. This test whether our model with predictors is better than a model with just an intercept( the null model)
 with(logmodel, null.deviance - deviance) # To get the test statistic
@@ -40,7 +50,9 @@ with(logmodel, df.null - df.residual) # To get the degree of freedom
 with(logmodel, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE)) # To get the p value
 # Our model is as a whole fits significantly better than an empty model
 
-## Multiple logistic regression
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Multiple logistic regression
 mlogmodel <- glm(hpt ~ exercise + hba1c, data=data, family=binomial)
 summary(mlogmodel)
 wald.test(b = coef(mlogmodel), Sigma = vcov(mlogmodel), Terms = 2:3)
@@ -51,7 +63,72 @@ with(mlogmodel, null.deviance - deviance)
 with(mlogmodel, df.null - df.residual) 
 with(mlogmodel, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE)) # To get the p value
 
-# After controlled for HbA1c, those who practice high exercise intensity have 0.07 lower odds compared to those in low exercise to get hypertension.
+# After controlled for HbA1c, those who practice high exercise intensity have 0.06 lower odds compared to those in low exercise to get hypertension.
 # This is also called the adjusted odd ratios.
+
+probabilities <- predict(mlogmodel, type = "response")
+predicted.classes <- ifelse(probabilities > 0.5, "TRUE", "FALSE")
+head(predicted.classes)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Logistic regression assumptions
+# 1. Linearity assumption
+# Here, we’ll check the linear relationship between continuous predictor variables and the logit of the outcome. 
+# This can be done by visually inspecting the scatter plot between each predictor and the logit values.
+# Select only numeric predictors
+data1 <- data %>% dplyr::select_if(is.numeric)
+predictors <- colnames(data1)
+# Bind the logit and tidying the data for plot
+data1 <- data1 %>%
+  mutate(logit = log(probabilities/(1-probabilities))) %>%
+  gather(key = "predictors", value = "predictor.value", -logit)
+# Create scatter plots
+ggplot(data1, aes(logit, predictor.value))+
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") + 
+  theme_bw() + 
+  facet_wrap(~predictors, scales = "free_y")
+# If is has linearity plot than it is ok
+# If the scatter plot shows non-linearity, you need other methods to build the model 
+# such as including 2 or 3-power terms, fractional polynomials and spline function 
+
+# 2. No influential values
+# Influential values are extreme individual data points that can alter the quality of the logistic regression model.
+#  The most extreme values in the data can be examined by visualizing the Cook’s distance values.
+# Here we label the top 3 largest values:
+plot(mlogmodel, which = 4, id.n = 3)
+# Note that, not all outliers are influential observations.
+# To check whether the data contains potential influential observations, the standardized residual error can be inspected.
+# Data points with an absolute standardized residuals above 3 represent possible outliers and may deserve closer attention.
+# The following R code computes the standardized residuals (.std.resid) and the Cook’s distance (.cooksd) using the R function augment()
+# Extract model results
+model.data <- augment(mlogmodel) %>% 
+  mutate(index = 1:n()) 
+# The data for the top 3 largest values, according to the Cook’s distance, can be displayed as follow:
+model.data %>% top_n(3, .cooksd)
+# Plot the standardized residuals
+ggplot(model.data, aes(index, .std.resid)) + 
+  geom_point(aes(color = hpt), alpha = .5) +
+  theme_bw()
+# Filter the infuential data points
+model.data %>% 
+  filter(abs(.std.resid) > 3)
+# There is no influential data in our points
+# When you have outliers in a continuous predictor, potential solutions include:
+# Removing the concerned data
+# Transform the data into log scale
+
+# 3. Multicollinearity
+mlogmodel2 <- glm(hpt ~ exercise + hba1c + age, data=data, family=binomial)
+car::vif(mlogmodel2)
+# As a rule of thumb, a VIF value that exceeds 5 or 10 indicates a problematic amount of collinearity. 
+# In our example, there is no collinearity: all variables have a value of VIF well below 5.
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
 # Table final result using apatables
+apa.reg.table(mlogmodel2, filename = "Table3_APA.doc", table.number = 3)
+
+
 
--- MAZA_MLR.R
+++ MAZA_MLR.R
@@ -1,78 +1,154 @@
+# Required package 
+install.packages("foreign")
+install.packages("rstatix")
+install.packages("MASS")
+install.packages("apaTables")
+library(foreign)
+library(rstatix)
+library(MASS)
+library(apaTables)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Data preparation 
 setwd("~/Desktop/Medical Statistic/IIUM Biostatistic Workshop/IIUM_Biostatistic_workshop")
-data <- read.spss(file="healthstatus.sav", to.data.frame=TRUE)
+data <- read.csv(file="healthstatus6.csv") 
 attach(data)
 str(data)
-data$bmi <- (data$wt / ((data$ht)/100)^2)
-hist(age)
 
-## Simple and Multiple Linear regression
-#Previously we have seen that there is a significant linear relationship between systolic blood pressure and HbA1C  
-attach(data)
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Simple Linear regression
+# Previously we have seen that there is a significant linear relationship between blood pressure and HbA1C  
+
 plot(hba1c, sbp, main="HbA1c versus Systolic blood pressure", xlab="HBA1C ", ylab="Systolic blood pressure ", pch=19)
 abline(lm(sbp~hba1c), col="red") # regression line (y~x) 
 cor.test(hba1c, sbp, method=c("pearson")) #correlation for normally distributed data
 
-# Now we can also observe the relationship between systolic blood pressure and HbA1c using simple linear regression
-# Simple linear regression also can predict
-# This relationship between 2 variable is stil bivariate analysis
-# But this relationship can be represented by this equation SBP = HBA1c * b1 + a
-# In this formula, a is the intercept and b is the slope, hence 1 unit increase in HBA1c,the SBP will increase by "b"
+plot(hba1c, dbp, main="HbA1c versus Diastolic blood pressure", xlab="HBA1C ", ylab="Diastolic blood pressure ", pch=19)
+abline(lm(dbp~hba1c), col="red") # regression line (y~x) 
+cor.test(hba1c, dbp, method=c("pearson")) #correlation for normally distributed data
 
+# Now we can also measure the linear relationship between each blood pressure and HbA1c using simple linear regression
+# This relationship between 2 variable is stil bivariate analysis
+# This relationship can be represented by the equation SBP = HBA1c * b1 + a
+# In this formula, a is the intercept and b is the slope, hence 1 unit increase in HBA1c,the SBP will increase by "b" coefficient unit
+# And similarly for Diastolic blood pressure 
 # A linear regression can be calculated in R with the command lm which stands for linear model
 # You can get the summary of the model using summary command
+# Compute the model and model summary
 lmsbp <- lm(sbp~hba1c,data=data)
 summary(lmsbp)
-lmsbp1 <- lm(sbp~sex,data=data)
-summary(lmsbp1)
+
+lmdbp <- lm(dbp~hba1c,data=data)
+summary(lmdbp)
+
+# You will get the a and b
+# a is the intercepts 122.9670 and b is the slope 1.3169
+# The coefficients estimates show the value for intercepts a and slope b value.
+# You can fit in the equation, SBP = HbA1c(1.3169) + 122.9670
+# So if a person with HbA1c value of 10, the systolic blood pressure is = 1.3169(10) + 122.9670 = 136.136 mmHg
+# Now interpret the p value for each coefficients 
+# For HbA1c which is 0.00021, the null null hypothesis is that b = 0 which mean that the population mean of Y is a for every x value, which tells us that x has no effect on Y. 
+# The p value of F-statistic tells whether your model provide better fit than a model with no predictors. Here the p value is 0.0002095 signficant which mean it is better with a predictor.
+# If your model is significant you can measure the strength of the relationship using adjusted R-squared
+# The R squared here 8.1% of the SBP variation can be explained by HBA1C
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# You can also apply categorical variable but this is essentially t-test for the hypothesis of the slope not equal to 0
+# But in linear regression you can add / control for other variable 
+lmsbp2 <- lm(sbp~sex,data=data)
+summary(lmsbp2)
 t.test(sbp ~ sex, data=data)
 
-# You will get the a and b, a is 116.534 and b is 1.052
-# The coefficients show the value for intercepts a and slope b value.
-# So if a person with HbA1c value of 10, the systolic blood pressure is = 1.05(10) + 116.5 = 127mmHg
-# Interpret the p value for each coffecients of HbA1c which is 0.0263, the null null hypothesis is that b = 0 which mean that the population mean of Y is a for every x value, which tells us that x has no effect on Y. 
-# The p value of F-statistic tells whether your model provide better fit than a model with no predictors. Here the p value is 0.026 signficant which mean it is better with a predictor.
-# If your model is significant you can measure the strength of the relationship using adjusted R-squared, here 2.6% of SBP variation can be explained by HBA1C
-
-## Multiple linear regression
+#-----------------------------------------------------------------------------------------------------------------------------
+  
+# Multiple linear regression
 # When a regression takes into account 2 or more predictors, its called multiple linear regression.
-# Yesterday we have seen that exercise is also significantly associated with SBP
+# Yesterday we have seen that exercise is also significantly associated with SBP when conducting anova
 # We can also add exercise in our model 
 # For example adding exercise in the model
-lmsbp1 <- lm(sbp~hba1c + exercise,data=data)
-summary(lmsbp1)
+lmsbp3 <- lm(sbp~hba1c+exercise,data=data)
+summary(lmsbp3)
 
-# The exercise is a categorical predictor, the reference here is low exercise, if on increase from low to moderate, the SBP reduce by 5.7mmHg on average.
-# The model omnibus p value is significant and the adjusted R-squared is better 0.07889. our model has become better. 
+# The exercise is a categorical predictor is treated as dummy variable, the reference here is high exercise, if one reduce from high to moderate, the SBP increase by 5.489 mmHg on average.
+# The model omnibus p value is significant and the adjusted R-squared is better 0.1579, thus our model has become better. 
 # We can recall the reference category being used by using contrasts command.
 # We can change the reference category using relevel function.
 contrasts(exercise)
 data <- within(data, 
-               exercise <- relevel(exercise, ref = "High"))
+               exercise <- relevel(exercise, ref = "Low"))
 contrasts(data$exercise)
 # Now i would like undo that.
 data <- within(data, 
-               exercise <- relevel(exercise, ref = "Low"))
+               exercise <- relevel(exercise, ref = "High"))
 # For exercise since it has more than 2 level, we can get the p value for overall exercise variable in which essentially an anova test
-Anova(lmsbp1)
-# Since it is significant, we can then further describe which pair are significant in which both are significant
-lmsbp1 <- lm(sbp~hba1c + exercise,data=data)
-summary(lmsbp1)
+Anova(lmsbp3)
+# Since it is significant 0.0005615, we can then further describe which pair are significant in which both are significant
+summary(lmsbp3)
 # We can also derived the 95% Confidence interval for the estimate using confint command
-confint(lmsbp1)
+confint(lmsbp3)
 
-# Linear regression makes few assumptions about the data 1.Linearity of the data. 2. Normality of residuals 3. Homogeneity of residuals variance 4. Independence of residuals error terms
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Identifying confounder
+# This also suggests a useful way of identifying confounding. 
+# Typically, we try to establish the association between a primary risk factor and a given outcome after adjusting for one or more other risk factors.
+# One useful strategy is to use multiple regression models to examine the association between the primary risk factor and the outcome before and after including possible confounding factors.
+# If the inclusion of a possible confounding variable in the model causes the association between the primary risk factor and the outcome to change by 10% or more
+# Then the additional variable is a confounder.
+# In first model, for every HbA1c 1 unit increase cause average increase of SBP by 1.3169
+# When we add exercise,for every HbA1C 1 unit increase cause average increase of SBP by 1.138
+# The changes was 0.1789/1.138 = 15.72%
+# So increase by more than 10% so it is a confounder, so we can controlled by using multiple linear regression
+# We can also include confounder variable from literature review
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# We can build better model by including confounder in our model
+# We can also building better model from a selection of variables using statistics
+# Stepwise Regression
+lmsbp4 <- lm(sbp~hba1c+sex+exercise+smoking,data=data)
+step <- stepAIC(lmsbp4, direction="both")
+step$anova # display results
+# Using AIC as the parameter, the model with smaller AIC is better
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Linear regression makes few assumptions about the data 
+# 1.Linearity of the data. 2. Normality of residuals 3. Homogeneity of residuals variance 4. Independence of residuals error terms
+
 # 1. The first one is the linearity of the data between predictor x and outcome y
 # This can be checked by inspecting Residuals vs Fitted 
 # Ideally, the residual plot will show no fitted pattern and the redline should be approximately horizontal at zero.
-plot(lmsbp1, 1)
-# 2. The normality of residuals must be normally distributed. This can be plot using Q-Q Plot. It’s good if residuals points follow the straight dashed line.
-plot(lmsbp1, 2)
-# 3. The scale location is used to check the homogeneity of variance of the residuals (homoscedasticity). Horizontal line with equally spread points is a good indication of homoscedasticity. 
-plot(lmsbp1, 3)
-# 4. The independence of residual error terms. Used to identify influential cases, that is extreme values that might influence the regression results when included or excluded from the analysis.
-# The Residuals vs Leverage plot can help us to find influential observations if any. On this plot, outlying values are generally located at the upper right corner or at the lower right corner. Those spots are the places where data points can be influential against a regression line.
-plot(lmsbp1, 5)
+plot(lmsbp, 1)
+# Note that, if the residual plot indicates a non-linear relationship in the data
+# Then a simple approach is to use non-linear transformations of the predictors, such as log(x), sqrt(x) and x^2, in the regression model.
 
-#How to summarize findings using apatable
+# 2. The normality of residuals must be normally distributed. This can be plot using Q-Q Plot.
+# It’s good if residuals points follow the straight dashed line.
+plot(lmsbp, 2)
+
+# 3. The scale location is used to check the homogeneity of variance of the residuals (homoscedasticity).
+# Horizontal line with equally spread points is a good indication of homoscedasticity. 
+plot(lmsbp, 3)
+# A possible solution to reduce the heteroscedasticity problem is to use a log or square root transformation of the outcome variable (y).
+# lmsbplog <- lm(log(sbp) ~ hba1c, data = data)
+# plot(lmsbplog, 3)
+
+# 4. No outliers 
+# The Residuals vs Leverage plot can help us to find influential observations if any. On this plot, outlying values are generally located at the upper right corner or at the lower right corner. 
+# Those spots are the places where data points can be influential against a regression line.
+# An outlier is a point that has an extreme outcome variable value.
+# Observations whose standardized residuals are greater than 3 in absolute value are possible outliers (James et al. 2014).
+# Here we don't have . If we have, we might want to remove that outliers 
+plot(lmsbp, 5)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#How to summarize findings using apaTable
+apa.reg.table(lmsbp4, filename = "Table1_APA.doc", table.number = 1) 
+apa.reg.table(lmsbp3, lmsbp4, filename = "Table2_APA.doc", table.number = 2)
 
 
