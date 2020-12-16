013e4a580942426b5dce5c4c0711c2a9f2433bb2
Jamalludin Ab Rahman <74943307+profjamal@users.noreply.github.com>
2020-12-16 23:58:43 +0800

    Update Intro to R.R

--- Intro to R.R
+++ Intro to R.R
@@ -1,14 +1,15 @@
 # Introduction to R
+setwd(getwd())
 
 # Data types
 
-ID <- 1:50
-Age <- rnorm(50, 40, 10)
+ID <- 1:100
+Age <- rnorm(100, 40, 10)
 head(Age)
 Age <- round(Age, digit=0)
-Sex <- rbinom(50, 1, .5)
+Sex <- rbinom(100, 1, .5)
 table(Sex)
-Sex <- factor(Sex, labels = c("Female", "Male"))
+Sex <- factor(Sex, levels = c(0,1), labels = c("Female", "Male"))
 table(Sex)
 data <- data.frame(cbind(ID, Age, Sex))
 head(data)
@@ -23,4 +24,21 @@ plot(Sex)
 
 plot(data)
 plot(Sex, Age)
+
+# Packages
+
+# Tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+
+data %>% 
+  group_by(Sex) %>% 
+  summarise(
+    N = n(),
+    Mean = mean(Age)
+  )
+
+
+ggplot(data, aes(Age))+
+  geom_histogram(fill="blue", alpha=.6, bins = 20)
 
